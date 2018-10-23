b47cec5a6e7c14c0aeabe304a79156105aeb62df
Saransh Sharma <saransh1337@gmail.com>
2018-10-24 03:38:47 +0530

    add toast button onclick listener

--- app/src/main/java/saransh1337/rednova/MainActivity.java
+++ app/src/main/java/saransh1337/rednova/MainActivity.java
@@ -3,6 +3,8 @@ package saransh1337.rednova;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d("MainActivity", "Red nova here !");
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+
     }
 }
