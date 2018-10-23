554145376dbe5013af6a5816aac847022a003abc
Saransh Sharma <saransh1337@gmail.com>
2018-10-24 03:50:02 +0530

    add clear counter capability

--- app/src/main/java/saransh1337/rednova/MainActivity.java
+++ app/src/main/java/saransh1337/rednova/MainActivity.java
@@ -4,23 +4,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount=0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("MainActivity", "Red nova here !");
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
     }
 
     public void showToast(View view) {
         Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        mCount=0;
+        if(mShowCount!=null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
         toast.show();
     }
 
     public void countUp(View view) {
 
+
+        mCount++;
+        if(mShowCount!=null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
